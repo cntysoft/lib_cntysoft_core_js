747dd014415d68174cc12be213657c3b616c3bf5
SOFTBOY <softboy@fhzc.com>
2016-01-13 20:34:25 +0800

    增加websocket类

---
+++ src/Framework/Net/WebSocket.js
@@ -0,0 +1,13 @@
+/*
+ * Cntysoft Cloud Software Team
+ *
+ * @author SOFTBOY <cntysoft@163.com>
+ * copyright  Copyright (c) 2010-2011 Cntysoft Technologies China Inc. <http://www.cntysoft.com>
+ * license    http://www.cntysoft.com/license/new-bsd     New BSD License
+ */
+/**
+ * WebSocket的简单封装
+ */
+Ext.define('Cntysoft.Framework.Net.WebSocket', {
+   
+});
